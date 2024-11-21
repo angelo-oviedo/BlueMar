9d63a9def6d251720295ddd0ab86420c48ad92bb
angelo-oviedo <angelooviedomont22@gmail.com>
2024-11-21 14:43:46 -0600

    RAW layer models definition and some definitions on Staging

---
+++ dags/dbt/BlueMar/models/raw/raw_gastos_operativos.sql
@@ -0,0 +1,19 @@
+with raw_gastos_operativos as (
+    SELECT
+        cantidad_,
+        concepto_,
+        descripcion_,
+        estado_factura,
+        factura,
+        fecha,
+        fecha_de_pago,
+        mes,
+        monto_unitario,
+        proveedor_,
+        referencia_banco,
+        total
+    FROM 
+        {{source('raw', 'raw_gastos_operativos')}}
+)
+
+SELECT * FROM raw_gastos_operativos
---
+++ dags/dbt/BlueMar/models/raw/raw_ingresos.sql
@@ -0,0 +1,16 @@
+with raw_ingresos as (
+        SELECT    
+            cantidad,
+            cliente,
+            estado_factura,
+            factura,
+            fecha,
+            mes,
+            precio_unitario,
+            referencia_banco,
+            talla,
+            total
+        FROM
+            {{source('raw', 'raw_ingresos')}}
+    )
+    SELECT * FROM raw_ingresos
